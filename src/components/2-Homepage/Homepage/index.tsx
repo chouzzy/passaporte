a9e97b1b6a824aa1c17e1ed9af71e8137eca3835
Matheus <mathfernando@hotmail.com>
2022-09-23 15:09:49 -0300

    img compression

--- src/components/2-Homepage/Homepage/index.tsx
+++ src/components/2-Homepage/Homepage/index.tsx
@@ -21,7 +21,7 @@ export function Homepage({serviceOn, backPage, servicePageData}:HomepageProps) {
    const backgrounds = ['bg1', 'bg2', 'bg3']
    return (
       <>
-         <Container id='#Home' maxW='100vw' minH={'100vh'} bg='clubDark' bgImage={`static/img/${background}.png`} bgPosition='center' bgSize='cover' p='0' m='0' centerContent>
+         <Container id='#Home' maxW='100vw' minH={'100vh'} bg='clubDark' bgImage={`static/img/test/${background}.png`} bgPosition='center' bgSize='cover' p='0' m='0' centerContent>
          <Navbar/>
          {serviceOn? <Banner bgBanner={backgrounds} bgState={setBackground}/> 
          :
