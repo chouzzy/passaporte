0446c298f79ac3461509cc28051fefcdd224c39e
mathf <Mathias>
2022-10-10 21:01:10 -0300

    atualização clube 3

--- src/components/3-Services/Visa/LineItem/index.tsx
+++ src/components/3-Services/Visa/LineItem/index.tsx
@@ -25,7 +25,7 @@ export function LineItem({LineImage, LineName, columns, lineData, changePage}:Li
          <Slink to={'#Home'}>
             <GridItem onClick={passLineData} mx='auto' w='100%' h='100%' _hover={{color:'clubMaldivas', transition: '300ms'}} bgSize='cover' bgPosition='center' py={[4,4,2,2]}>
                
-               <Image src={LineImage} mx='auto'  _hover={{outline:'1px solid #4ca7a1', transition:'400ms', boxShadow:'dark-lg'}} boxSize={[40,40,40,48]} borderRadius={'100%'} objectFit='cover'/>
+               <Image src={LineImage} mx='auto'  _hover={{outline:'1px solid #4ca7a1', transition:'400ms', boxShadow:'dark-lg'}} boxSize={[32,40,40,48]} borderRadius={'100%'} objectFit='cover'/>
                
                <Flex maxW={72} py={2} justifyContent={'center'} mx='auto' 
                fontSize={['0.8rem','0.8rem','1rem','1.1rem']}>
