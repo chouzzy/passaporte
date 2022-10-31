a3121f3b69499436c4c7a95e0e70bdb2d0089c49
mathf <Mathias>
2022-10-31 13:52:33 -0300

    ultimas atlreações 2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
                <noscript
                   dangerouslySetInnerHTML={{
                      __html: `
-                     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WN8JSC"
+                     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-"
                      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
                   }}
                />
